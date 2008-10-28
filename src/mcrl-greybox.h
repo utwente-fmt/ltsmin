943bae3992ba9a51fe06cbe159fbe731d88134d1
Stefan Blom <sccblom@cs.utwente.nl>
2008-10-28 21:44:47 +0100

    new tool in progress: lpo2lts-grey + 1 file

---
+++ src/mcrl-greybox.h
@@ -0,0 +1,11 @@
+#ifndef MCRL_GREYBOX_H
+#define MCRL_GREYBOX_H
+
+#include "greybox.h"
+
+extern void MCRLinitGreybox(int argc,char *argv[],void* stack_bottom);
+/**< Initialize ATerm library and MCRL library up to the loading of a specification. */
+
+#endif
+
+
