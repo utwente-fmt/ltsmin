cee1f96cf92630a1463dc7117b78a9e9b3948166
Michael Weber <michaelw@cs.utwente.nl>
2009-08-15 19:37:07 +0200

    lemon parser setup, part 2/2
    Required patch for lemon.c

--- lemon/lemon.c
+++ lemon/lemon.c
@@ -2709,23 +2709,29 @@ struct plink *plp;
 ** Procedures for generating reports and tables in the LEMON parser generator.
 */
 
-/* Generate a filename with the given suffix.  Space to hold the
-** name comes from malloc() and must be freed by the calling
-** function.
+/* Generate a filename with the given suffix (without the directory
+** part).  Space to hold the name comes from malloc() and must be
+** freed by the calling function.
 */
 PRIVATE char *file_makename(lemp,suffix)
 struct lemon *lemp;
 char *suffix;
 {
   char *name;
+  char *basename;
   char *cp;
 
-  name = malloc( lemonStrlen(lemp->filename) + lemonStrlen(suffix) + 5 );
+  basename = strrchr (lemp->filename, '/');
+  if (basename != NULL)
+      ++basename;
+  else
+      basename = lemp->filename;
+  name = malloc( lemonStrlen(basename) + lemonStrlen(suffix) + 5 );
   if( name==0 ){
     fprintf(stderr,"Can't allocate space for a filename.\n");
     exit(1);
   }
-  strcpy(name,lemp->filename);
+  strcpy(name, basename);
   cp = strrchr(name,'.');
   if( cp ) *cp = 0;
   strcat(name,suffix);
