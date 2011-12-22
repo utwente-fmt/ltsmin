fbaecd7767c624cfda2b52ab02586349a4996270
Stefan Blom <sccblom@xs4all.nl>
2011-12-22 13:58:09 +0100

    patch strong bisimulation for kripke frames

--- src/lts-lib/lowmem_s.c
+++ src/lts-lib/lowmem_s.c
@@ -7,9 +7,12 @@
 #include <lts-lib/lts.h>
 #include <lts-lib/lowmem.h>
 
+static int has_labels;
+
 static int samesig(lts_t lts,int*map,int s1,int s2){
     uint32_t t1,t2,m1,m2,t;
 
+    if(map[s1]!=map[s2]) return 0;
     t1=lts->begin[s1];
     t2=lts->begin[s2];
     m1=lts->begin[s1+1];
@@ -17,15 +20,15 @@ static int samesig(lts_t lts,int*map,int s1,int s2){
     for(;;){
         if(t1==m1) return (t2==m2);
         if(t2==m2) return 0;
-        if(lts->label[t1]!=lts->label[t2]) return 0;
+        if(has_labels && lts->label[t1]!=lts->label[t2]) return 0;
         if(map[lts->dest[t1]]!=map[lts->dest[t2]]) return 0;
         t=t1+1;
-        while(t<m1 && lts->label[t1]==lts->label[t] && map[lts->dest[t]]==map[lts->dest[t1]]){
+        while(t<m1 && (!has_labels  || lts->label[t1]==lts->label[t]) && map[lts->dest[t]]==map[lts->dest[t1]]){
             t++;
         }
         t1=t;
         t=t2+1;
-        while(t<m2 && lts->label[t2]==lts->label[t] && map[lts->dest[t]]==map[lts->dest[t2]]){
+        while(t<m2 && (!has_labels  || lts->label[t2]==lts->label[t]) && map[lts->dest[t]]==map[lts->dest[t2]]){
             t++;
         }
         t2=t;
@@ -35,15 +38,16 @@ static int samesig(lts_t lts,int*map,int s1,int s2){
 
 static int hashcode(lts_t lts,int *map,int i){
     int hc;
-    unsigned long j;
-    hc=0;
+    long j;
+    hc=map[i];
     for(j=lts->begin[i];j<lts->begin[i+1];j++){
         if(j>lts->begin[i] &&
-            lts->label[j]==lts->label[j-1] &&
+            (!has_labels  || lts->label[j]==lts->label[j-1]) &&
             map[lts->dest[j]]==map[lts->dest[j-1]] ){
             continue;
         }
-        hc=hc+12531829*lts->label[j]+87419861*map[lts->dest[j]];
+        if (has_labels) hc+=12531829*lts->label[j];
+        hc+=87419861*map[lts->dest[j]];
     }
     return hc;
 }
@@ -53,6 +57,8 @@ static int rehash(int h){
 }
 
 void lowmem_strong_reduce(lts_t lts){
+    has_labels=lts->label!=NULL;
+    int has_props=lts->properties!=NULL;
     int *map,*newmap,*tmpmap;
     int *hash,mask,hc;
     int tmp, j;
@@ -61,11 +67,18 @@ void lowmem_strong_reduce(lts_t lts){
     long long int chain_length;
     long long int hash_lookups;
     uint32_t m, k;
+
     lts_set_type(lts,LTS_BLOCK);
     map=(int*)RTmalloc(lts->states*sizeof(int));
     newmap=(int*)RTmalloc(lts->states*sizeof(int));
-    for(i=0;i<lts->states;i++){
-        map[i]=0;
+    if (has_props){
+        for(i=0;i<lts->states;i++){
+            map[i]=lts->properties[i];
+        }
+    } else {
+        for(i=0;i<lts->states;i++){
+            map[i]=0;
+        }
     }
     for(i=1<<18;i<lts->states;i=i<<1){
     }
@@ -83,11 +96,13 @@ void lowmem_strong_reduce(lts_t lts){
         for(i=0;i<lts->states;i++){
             for(m=lts->begin[i];m<lts->begin[i+1];m++){
                 for(k=m;k>lts->begin[i];k--){
-                    if(lts->label[k]>lts->label[k-1]) break;
-                    if((lts->label[k]==lts->label[k-1])&&(map[lts->dest[k]]>=map[lts->dest[k-1]])) break;
-                    tmp=lts->label[k];
-                    lts->label[k]=lts->label[k-1];
-                    lts->label[k-1]=tmp;
+                    if (has_labels){
+                        if(lts->label[k]>lts->label[k-1]) break;
+                        if((lts->label[k]==lts->label[k-1])&&(map[lts->dest[k]]>=map[lts->dest[k-1]])) break;
+                        tmp=lts->label[k];
+                        lts->label[k]=lts->label[k-1];
+                        lts->label[k-1]=tmp;
+                    } else if (map[lts->dest[k]]>=map[lts->dest[k-1]]) break;
                     tmp=lts->dest[k];
                     lts->dest[k]=lts->dest[k-1];
                     lts->dest[k-1]=tmp;
@@ -147,25 +162,48 @@ void lowmem_strong_reduce(lts_t lts){
         newmap=tmpmap;
     }
     Print(infoLong,"Average hash chain length: %2.3f",((float)chain_length)/((float)hash_lookups));
-    lts_set_type(lts,LTS_LIST);
     count=0;
     for(i=0;i<lts->states;i++){
         if(map[i]==(int)i){
             newmap[i]=count;
             count++;
+        } else {
+            newmap[i]=newmap[map[i]];
         }
     }
-    lts->states=oldcount;
-    for(i=0;i<lts->root_count;i++){
-        lts->root_list[i]=newmap[map[lts->root_list[i]]];
-    }
-    for(m=0;m<lts->transitions;m++){
-        lts->src[m]=newmap[map[lts->src[m]]];
-        lts->dest[m]=newmap[map[lts->dest[m]]];
-    }
     free(map);
+    lts_quotient(lts,newmap);
     free(newmap);
-    lts_uniq(lts);
 }
 
+void lts_quotient(lts_t lts,uint32_t *map){
+    int has_props=lts->properties!=NULL;
+    lts_set_type(lts,LTS_LIST);
+    uint32_t *temp_props=NULL;
+    if(has_props){
+        temp_props=(uint32_t*)RTmalloc(lts->states*4);
+    }
+    uint32_t new_states=0;
+    for(uint32_t i=0;i<lts->states;i++){
+        if(has_props) temp_props[map[i]]=lts->properties[i];
+        if(map[i]>=new_states) {
+            new_states=map[i]+1;
+        }
+    }
+    lts_set_size(lts,lts->root_count,new_states,lts->transitions);
+    if (has_props){
+        for(uint32_t i=0;i<lts->states;i++){
+            lts->properties[i]=temp_props[i];
+        }
+        RTfree(temp_props);
+    }
+    for(uint32_t i=0;i<lts->root_count;i++){
+        lts->root_list[i]=map[lts->root_list[i]];
+    }
+    for(uint32_t m=0;m<lts->transitions;m++){
+        lts->src[m]=map[lts->src[m]];
+        lts->dest[m]=map[lts->dest[m]];
+    }
+    lts_uniq(lts);
+}
 
