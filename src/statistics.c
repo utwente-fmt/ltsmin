5b9503499658077eaff311f7fb08f1b5c49220c0
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2012-05-01 22:19:15 +0200

    Add statistics functionality

--- src/statistics.c
+++ src/statistics.c
@@ -1,5 +1,6 @@
 
 #include <math.h>
+#include <runtime.h>
 
 #include <statistics.h>
 
@@ -15,6 +16,34 @@ statistics_record (statistics_t *stats, double x) {
         stats->Qk += (stats->k-1)*d*d/stats->k;
         stats->Mk += d/stats->k;
     }
+}
+
+void
+statistics_unrecord (statistics_t *stats, double x) {
+    if (0 == stats->k) {
+        Abort ("Too many unrecords: statistics_unrecord");
+    } else if (1 == stats->k) {
+        stats->Qk = 0;
+        stats->Mk = 0;
+    } else {
+        double d = stats->Mk * stats->k - x;
+        stats->Mk = d / (stats->k - 1);
+        d = (x - stats->Mk);  // is now: xk - M_{k-1}
+        stats->Qk = (stats->Qk - (stats->k - 1)*d*d)/stats->k;
+    }
+    stats->k--;
+}
+
+void
+statistics_union (statistics_t *out, statistics_t *s1, statistics_t *s2)
+{
+    size_t k1 = s1->k;
+    size_t k2 = s2->k;
+    out->k = k1 + k2;
+    double Mk1 = s1->Mk;
+    double Mk2 = s2->Mk;
+    out->Mk = (k1 * Mk1 + k2 * Mk2) / out->k;
+    out->Qk = (s1->Qk + Mk1*Mk1 + s2->Qk + Mk2*Mk2) - out->Mk*out->Mk;
 }
 
 double
@@ -51,5 +80,7 @@ void
 statistics_init (statistics_t *stats)
 {
     stats->k = 0;
+    stats->Qk = 0;
+    stats->Mk = 0;
 }
 
--- src/statistics.h
+++ src/statistics.h
@@ -8,6 +8,8 @@
  * [1] Mark Hoemmen - "Computing the standard deviation efficiently"
  * (http://www.cs.berkeley.edu/~mhoemmen/cs194/Tutorials/variance.pdf)
  *
+ * I added statistics_unrecord to roll back recorded samples.
+ * And statistics_union to combine two stat records.
  *
  * Alfons
  */
@@ -25,6 +27,11 @@ typedef struct statistics_s {
 } statistics_t;
 
 extern void statistics_record (statistics_t *stats, double x);
+extern void statistics_unrecord (statistics_t *stats, double x);
+/**
+ * Unionizes two stats. Maybe inline: s1 = out \/ s2 = out
+ */
+extern void statistics_union (statistics_t *out, statistics_t *s1, statistics_t *s2);
 extern double statistics_mean (statistics_t *stats);
 extern double statistics_stdev (statistics_t *stats);
 extern size_t statistics_nsamples (statistics_t *stats);
