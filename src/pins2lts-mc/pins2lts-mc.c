097bd9068c9d672558cc0cc21dbfa383082875f1
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2012-11-19 19:38:31 +0100

    Limit locking and added documentation OWCTY

--- src/pins2lts-mc/pins2lts-mc.c
+++ src/pins2lts-mc/pins2lts-mc.c
@@ -923,9 +923,13 @@ local_init ()
 
 #if SPEC_MT_SAFE == 1
     // some frontends (opaal) do not have a thread-safe initial state function
+#ifdef OPAAL
     pthread_mutex_lock (&global->mutex);
+#endif
     GBloadFile (model, files[0], &model);
+#ifdef OPAAL
     pthread_mutex_unlock (&global->mutex);
+#endif
 
     if (HREme(HREglobal()) == 0)
         statics_init (model);
@@ -2478,6 +2482,23 @@ sbfs (wctx_t *ctx)
 
 /**
  * OWCTY-MAP Barnat et al.
+ *
+ * Input-output diagram of OWCTY procedures:
+ *
+ *      reach(init)         swap          (reset)       reach
+ * stack--------> out_stack ----> in_stack ----> stack ------> out_stack
+ *                                   ^  [pre>0;pre=0]  [pre++] /| [pre==0]
+ *                                    \_______________________/ |pre_elimination
+ *                                       pre_elimination        v
+ *                                            [pre>0]        stack-\ [--pre==0]
+ *                                                             ^___/ elimination
+ *
+ * Only accepting states are transfered between methods.
+ * The predecessor count (pre) is kept in a global array (global->pre). It also
+ * includes a flip bit to distinguish new states in each iteration of
+ * reachability. By doing a compare and swap on pre + flip, we can grab
+ * states with accepting predecessors and reset pre = 0, hence
+ * reset van be inlined in reach(ability).
  */
 
 #define                     state_ext32 ((uint32_t *)global->pre)
