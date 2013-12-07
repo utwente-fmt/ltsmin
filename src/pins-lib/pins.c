7d6908275a33bc1e70ff97d01a330273f1bf06af
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2013-12-08 00:29:53 +0100

    Fix PINS thread-safe abort

--- src/pins-lib/pins.c
+++ src/pins-lib/pins.c
@@ -1007,19 +1007,22 @@ GBloadFile (model_t model, const char *filename, model_t *wrapped)
                         if (log_active(infoLong)) {
                             GBprintPORMatrix(stdout, model);
                         }
+                        HREbarrier (HREglobal());
                         HREabort (LTSMIN_EXIT_SUCCESS);
                     }
+                    HREbarrier (HREglobal());
                 } else if (labels) {
                     if (HREme(HREglobal()) == 0) {
                         if (log_active(info)){
                             lts_type_printf(info, GBgetLTStype(model));
                         }
                         chunk_table_print(info, model);
+                        HREbarrier (HREglobal());
                         HREabort (LTSMIN_EXIT_SUCCESS);
                     }
-                } else {
-                    return;
+                    HREbarrier (HREglobal());
                 }
+                return;
             }
         }
         Abort("No factory method has been registered for %s models",
