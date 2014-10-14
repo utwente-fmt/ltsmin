614cb850a4c462221daa40f845471690b6e89eb1
Jeroen Meijer <jjgmeijer@gmail.com>
2014-10-15 00:25:40 +0300

    now also install ltsmin-standard.h and remove need for installing hre

--- src/ltsmin-lib/ltsmin-standard.h
+++ src/ltsmin-lib/ltsmin-standard.h
@@ -11,16 +11,13 @@
 #ifndef LTSMIN_STANDARD_H_
 #define LTSMIN_STANDARD_H_
 
-#include <hre/user.h>
-
-
 /**
  * Exit codes
  */
 
 #define LTSMIN_EXIT_COUNTER_EXAMPLE     1
-#define LTSMIN_EXIT_SUCCESS             HRE_EXIT_SUCCESS
-#define LTSMIN_EXIT_FAILURE             HRE_EXIT_FAILURE
+#define LTSMIN_EXIT_SUCCESS             0
+#define LTSMIN_EXIT_FAILURE             255
 
 /**
  * Matrices
