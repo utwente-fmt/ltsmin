c1eb8a2c9a8b60f402127d6a341aaf0d513f10bc
Vincent Bloemen <v.bloemen@student.utwente.nl>
2015-03-24 11:10:47 +0100

    Added atomic read/write operations

--- src/pins2lts-mc/parallel/unionfind.h
+++ src/pins2lts-mc/parallel/unionfind.h
@@ -10,22 +10,7 @@
  * TODO: documentation
  */
 
-typedef uint64_t    sz_w;
 
-typedef struct uf_node_s {
-    ref_t           parent;         // The parent in the UF tree
-    unsigned char   rank;           // The height of the UF tree
-    sz_w            w_set;          // Set of worker IDs (one bit for each worker)
-    char            uf_status;      // {UNSEEN, INIT, LIVE, LOCKED, DEAD}
-    char            list_status;    // {LIVE, BUSY, REMOVED}
-    ref_t           list_next;      // next list `pointer' (we could also use a standard pointer)
-} uf_node_t;
-
-
-struct uf_s {
-    uf_node_t      *array;   // array: [ref_t] -> uf_node
-    bool finished;
-};
 typedef struct uf_s uf_t;
 
 #define PICK_DEAD       1
@@ -47,7 +32,7 @@ extern void     uf_remove_from_list (const uf_t* uf, ref_t state);
 
 extern bool     uf_is_in_list (const uf_t* uf, ref_t state);
 
-extern char     uf_make_claim (const uf_t* uf, ref_t state, sz_w w_id);
+extern char     uf_make_claim (const uf_t* uf, ref_t state, size_t w_id);
 
 extern void     uf_merge_list(const uf_t* uf, ref_t list_x, ref_t list_y);
 
@@ -74,8 +59,6 @@ extern void      uf_unlock (const uf_t* uf, ref_t state);
 // testing
 
 extern bool      uf_mark_undead (const uf_t* uf, ref_t state);
-
-extern char     *uf_get_str_w_set(sz_w w_set);
 
 extern void      uf_debug (const uf_t* uf, ref_t state);
 
