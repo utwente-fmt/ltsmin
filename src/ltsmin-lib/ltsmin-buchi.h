90793d3ae4e438140488459801d60fe88f08ea27
Vincent Bloemen <v.bloemen@utwente.nl>
2016-01-20 14:25:24 +0100

    Extend ltsmin_buchi_t for TGBA

--- src/ltsmin-lib/ltsmin-buchi.h
+++ src/ltsmin-lib/ltsmin-buchi.h
@@ -9,6 +9,7 @@ typedef struct ltsmin_buchi_transition {
     int* pos; // * copy sym_table to point to ltsmin_expressions? */
     int* neg;
     int to_state;
+    uint32_t acc_set;       // TGBA acceptance marks
     int index;  // A global unique dense trans index in the LTL buchi automaton
 } ltsmin_buchi_transition_t;
 
@@ -19,6 +20,7 @@ typedef struct ltsmin_buchi_state {
 } ltsmin_buchi_state_t;
 
 typedef struct ltsmin_buchi {
+    uint32_t acceptance_set;      // TGBA acceptance set
     int predicate_count;
     ltsmin_expr_t* predicates;
     int state_count;
