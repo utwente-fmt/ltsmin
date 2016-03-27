a3d82c5e717fdecb9190b5d3cf108ee760b370ab
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2016-03-27 19:41:26 +0200

    Remove UseGuards stuff from PINS
    Preserves the code as deprecated wrapper.

---
+++ src/pins-lib/pins2pins-guards.h
@@ -0,0 +1,21 @@
+#ifndef PINS2PINS_GUARDS
+#define PINS2PINS_GUARDS
+
+#include <popt.h>
+
+#include <pins-lib/pins.h>
+
+extern struct poptOption guards_options[];
+
+extern int              PINS_USE_GUARDS;
+
+/**
+\brief Add wrapper that checks guards for next_all calls.
+*
+* This is a deprecated wrapper. Faster guard-based implementations of the
+* next-all function can better be achieved in the language module itself.
+* The --pins-guards flag is therefore hidden.
+*/
+extern model_t GBaddGuards (model_t model);
+
+#endif // PINS2PINS_GUARDS
