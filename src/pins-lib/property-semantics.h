c7a8099f8bc25c0db03f8b1358a8e50d8538308a
Jeroen Meijer <jjgmeijer@gmail.com>
2016-04-03 22:17:41 +0200

    property-semantics: add support for inequalities and arithmatic

--- src/pins-lib/property-semantics.h
+++ src/pins-lib/property-semantics.h
@@ -8,10 +8,13 @@
 #ifndef PROPERTY_SEMANTICS_H
 #define PROPERTY_SEMANTICS_H
 
+#include <limits.h>
+
 #include <ltsmin-lib/ltsmin-tl.h>
-#include <pins-lib/pins.h>
 #include <ltsmin-lib/ltsmin-parse-env.h>
 #include <ltsmin-lib/ltsmin-standard.h>
+#include <pins-lib/pins.h>
+#include <util-lib/util.h>
 
 extern ltsmin_expr_t parse_file(const char *file, parse_f parser, model_t model);
 
@@ -29,7 +32,7 @@ extern void mark_visible(model_t model, ltsmin_expr_t e, ltsmin_parse_env_t env)
  * NUM values may be looked up in the chunk tables (for chunk types), hence the
  * conditional.
  */
-static inline int
+static inline long
 eval_predicate(model_t model, ltsmin_expr_t e, transition_info_t *ti, int *state,
                int N, ltsmin_parse_env_t env)
 {
@@ -56,20 +59,80 @@ eval_predicate(model_t model, ltsmin_expr_t e, transition_info_t *ti, int *state
         case PRED_NOT:
             return !eval_predicate(model, e->arg1, ti, state, N, env);
         case PRED_EQ:
-            return (eval_predicate(model, e->arg1, ti, state, N, env) ==
-                    eval_predicate(model, e->arg2, ti, state, N, env));
+            return eval_predicate(model, e->arg1, ti, state, N, env) ==
+                    eval_predicate(model, e->arg2, ti, state, N, env);
+        case PRED_NEQ:
+            return eval_predicate(model, e->arg1, ti, state, N, env) !=
+                    eval_predicate(model, e->arg2, ti, state, N, env);
         case PRED_AND:
-            return (eval_predicate(model, e->arg1, ti, state, N, env) &&
-                    eval_predicate(model, e->arg2, ti, state, N, env));
+            return eval_predicate(model, e->arg1, ti, state, N, env) &&
+                    eval_predicate(model, e->arg2, ti, state, N, env);
         case PRED_OR:
-            return (eval_predicate(model, e->arg1, ti, state, N, env) ||
-                    eval_predicate(model, e->arg2, ti, state, N, env));
+            return eval_predicate(model, e->arg1, ti, state, N, env) ||
+                    eval_predicate(model, e->arg2, ti, state, N, env);
         case PRED_IMPLY:
-            return ((!eval_predicate(model, e->arg1, ti, state, N, env)) ||
-                      eval_predicate(model, e->arg2, ti, state, N, env));
+            return !eval_predicate(model, e->arg1, ti, state, N, env) ||
+                      eval_predicate(model, e->arg2, ti, state, N, env);
         case PRED_EQUIV:
-            return ((!eval_predicate(model, e->arg1, ti, state, N, env)) ==
-                    (!eval_predicate(model, e->arg2, ti, state, N, env)) );
+            return !eval_predicate(model, e->arg1, ti, state, N, env) ==
+                    !eval_predicate(model, e->arg2, ti, state, N, env);
+        case PRED_LT:
+            return eval_predicate(model, e->arg1, ti, state, N, env) <
+                    eval_predicate(model, e->arg2, ti, state, N, env);
+        case PRED_LEQ:
+            return eval_predicate(model, e->arg1, ti, state, N, env) <=
+                    eval_predicate(model, e->arg2, ti, state, N, env);
+        case PRED_GT:
+            return eval_predicate(model, e->arg1, ti, state, N, env) >
+                    eval_predicate(model, e->arg2, ti, state, N, env);
+        case PRED_GEQ:
+            return eval_predicate(model, e->arg1, ti, state, N, env) >=
+                    eval_predicate(model, e->arg2, ti, state, N, env);
+        case PRED_MULT: {
+            const long l = eval_predicate(model, e->arg1, ti, state, N, env);
+            const long r = eval_predicate(model, e->arg2, ti, state, N, env);
+            if (long_mult_overflow(l, r)) {
+                LTSminLogExpr (error, "integer overflow in: ", e, env);
+                HREabort(LTSMIN_EXIT_FAILURE);
+            }
+            return l * r;
+        }
+        case PRED_DIV: {
+            const long l = eval_predicate(model, e->arg1, ti, state, N, env);
+            const long r = eval_predicate(model, e->arg2, ti, state, N, env);
+            if (r == 0 || ((l == LONG_MIN) && r == -1)) {
+                LTSminLogExpr (error, "division by zero in: ", e, env);
+                HREabort(LTSMIN_EXIT_FAILURE);
+            }
+            return l / r;
+        }
+        case PRED_REM: {
+            const long l = eval_predicate(model, e->arg1, ti, state, N, env);
+            const long r = eval_predicate(model, e->arg2, ti, state, N, env);
+            if (r == 0 || ((l == LONG_MIN) && r == -1)) {
+                LTSminLogExpr (error, "division by zero in: ", e, env);
+                HREabort(LTSMIN_EXIT_FAILURE);
+            }
+            return l % r;
+        }
+        case PRED_ADD: {
+            const long l = eval_predicate(model, e->arg1, ti, state, N, env);
+            const long r = eval_predicate(model, e->arg2, ti, state, N, env);
+            if ((r > 0 && l > LONG_MAX - r) || (r < 0 && l < LONG_MIN - r)) {
+                LTSminLogExpr (error, "integer overflow in: ", e, env);
+                HREabort(LTSMIN_EXIT_FAILURE);
+            }
+            return l + r;
+        }
+        case PRED_SUB: {
+            const long l = eval_predicate(model, e->arg1, ti, state, N, env);
+            const long r = eval_predicate(model, e->arg2, ti, state, N, env);
+            if ((r > 0 && l < LONG_MIN + r) || (r < 0 && l > LONG_MAX + r)) {
+                LTSminLogExpr (error, "integer overflow in: ", e, env);
+                HREabort(LTSMIN_EXIT_FAILURE);
+            }            
+            return l - r;
+        }
         default:
             LTSminLogExpr (error, "Unhandled predicate expression: ", e, env);
             HREabort (LTSMIN_EXIT_FAILURE);
