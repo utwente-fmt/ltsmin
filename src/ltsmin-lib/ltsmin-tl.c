280cd802e89443ecbcacfda0fe12de11796908d7
Jeroen Meijer <jjgmeijer@gmail.com>
2016-04-12 21:10:29 +0200

    ltsmin-tl: explicitly note we do not support symbolic variables.

--- src/ltsmin-lib/ltsmin-tl.c
+++ src/ltsmin-lib/ltsmin-tl.c
@@ -631,6 +631,9 @@ pred_tree_walk(ltsmin_expr_t e, ltsmin_parse_env_t env, lts_type_t lts_type)
                     e->annotation->chunk_type = type_check_get_type(lts_type, LTSMIN_TYPE_BOOL, e, env);
                     return e;
                 }
+                case PRED_VAR:
+                    LTSminLogExpr (error, "Symbolic variables are not supported semantically: ", e, env);
+                    HREabort (LTSMIN_EXIT_FAILURE);
                 default: {
                     LTSminLogExpr (error, "Unhandled predicate expression: ", e, env);
                     HREabort (LTSMIN_EXIT_FAILURE);
