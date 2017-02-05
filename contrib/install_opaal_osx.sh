654afc7527ffba2eb3a5426b62ef68d9fa2126b4
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2017-02-05 16:17:49 +0100

    Update OPAAL script

--- contrib/install_opaal_osx.sh
+++ contrib/install_opaal_osx.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-# Required: bzr, wget, tar, Python (tested with 2.7), py-mpi4pi, boost, swig, gcc
+# A script for installing OPAAL under OSX. Since the later OSX versions swap
+# the default c++ standard library, the pydbm package crashes because of an
+# undefined symbol. This is solved here by compiling the package with a
+# different library (libstdc++).
+#
+# The script installs OPAAL and additional libraries in ./opaal, where it
+# should remain (this folder cannot be moved). The current directory is
+# used for temporary storage as well (which is deleted, unless the scripts
+# fails with an error or REMOVE is set to anything else than 1).
+#
+# The script is based on Andreas Dalsgaard's compilation script for OPAAL
+# and LTSmin, which can be found at: 
+# http://opaal-modelchecker.com/opaal-ltsmin/
+# http://mcc.uppaal.org/opaal/install_opaal_ltsmin-1.0.15
+#
+# Required packages:
+# bzr, wget, tar, Python (tested with 2.7), py-mpi4pi, boost, swig, gcc
+
 
 #extra search path for libraries in OSX, ignore on other OSs
 OSX="/opt/local"
-REMOVE=0
+REMOVE=1
 
 set -e
 
