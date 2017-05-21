7af255e235b777d5f5f19353f9c0323c86fbdce7
Tom van Dijk <t.vandijk@gmail.com>
2017-05-21 12:31:30 +0200

    vset_sylvan: now supports saturation (sat, sat-fix) strategies

--- src/vset-lib/vset_sylvan.c
+++ src/vset-lib/vset_sylvan.c
@@ -11,6 +11,7 @@
 #include <hre-io/user.h>
 #include <vset-lib/vdom_object.h>
 #include <sylvan.h>
+#include <sylvan_int.h>
 
 static int xstatebits = 16;   // maximum / default bits per integer
 static int xactionbits = 16;  // maximum / default bits per action variable
@@ -1076,6 +1077,109 @@ rel_update(vrel_t dst, vset_t src, vrel_update_cb cb, void* context)
     mtbdd_refs_popptr(2);
 }
 
+/**
+ * Implementation of (parallel) saturation.
+ * Assumes: set is of long vectors; relations are ordered on first variable.
+ */
+TASK_4(BDD, go_sat, BDD, set, vrel_t*, rels, int, count, int, id)
+{
+    /* Terminal cases */
+    if (set == sylvan_false) return sylvan_false;
+    if (count == 0) return set;
+
+    /* Consult the cache */
+    BDD result;
+    BDD _set = set;
+    if (cache_get3(200LL<<40, _set, (uint64_t)rels, id, &result)) return result;
+    mtbdd_refs_pushptr(&_set);
+
+    /* Check if the relation should be applied */
+    uint32_t var = sylvan_var(rels[0]->all_variables);
+    if (set == sylvan_true || var <= sylvan_var(set)) {
+        /* Count the number of relations starting here */
+        int n = 1;
+        while (n < count && var == sylvan_var(rels[n]->all_variables)) n++;
+        /*
+         * Compute until fixpoint:
+         * - SAT deeper
+         * - learn and chain-apply all current level once
+         */
+        BDD prev = sylvan_false;
+        BDD step = sylvan_false;
+        struct vector_set dummy;
+        mtbdd_refs_pushptr(&set);
+        mtbdd_refs_pushptr(&prev);
+        mtbdd_refs_pushptr(&step);
+        while (prev != set) {
+            prev = set;
+            // SAT deeper
+            set = CALL(go_sat, set, rels+n, count-n, id);
+            // learn and chain-apply all current level once
+            for (int i=0;i<n;i++) {
+                if (rels[i]->expand != NULL) {
+                    // project set
+                    dummy.dom = rels[i]->dom;
+                    dummy.k = rels[i]->r_k;
+                    dummy.proj = rels[i]->r_proj;
+                    dummy.state_variables = rels[i]->state_variables;
+                    dummy.bdd = sylvan_project(set, dummy.state_variables);
+                    // call expand callback
+                    mtbdd_refs_pushptr(&dummy.bdd);
+                    rels[i]->expand(rels[i], &dummy, rels[i]->expand_ctx);
+                    mtbdd_refs_popptr(1);
+                }
+                // and then step
+                step = sylvan_relnext(set, rels[i]->bdd, rels[i]->all_variables);
+                set = sylvan_or(set, step);
+                step = sylvan_false; // unset, for gc
+            }
+        }
+        mtbdd_refs_popptr(3);
+        result = set;
+    } else {
+        /* Recursive computation */
+        mtbdd_refs_spawn(SPAWN(go_sat, sylvan_low(set), rels, count, id));
+        BDD high = mtbdd_refs_push(CALL(go_sat, sylvan_high(set), rels, count, id));
+        BDD low = mtbdd_refs_sync(SYNC(go_sat));
+        mtbdd_refs_pop(1);
+        result = sylvan_makenode(sylvan_var(set), low, high);
+    }
+    // Store in cache
+    cache_put3(200LL<<40, _set, (uint64_t)rels, id, result);
+    mtbdd_refs_popptr(1);
+    return result;
+}
+
+static void
+set_least_fixpoint(vset_t dst, vset_t src, vrel_t _rels[], int rel_count)
+{
+    // Create copy of rels
+    vrel_t rels[rel_count];
+    memcpy(rels, _rels, sizeof(vrel_t[rel_count]));
+
+    // Sort the rels (using gnome sort)
+    int i = 1, j = 2;
+    vrel_t t;
+    while (i < rel_count) {
+        vrel_t *p = rels+i, *q = p-1;
+        if (sylvan_var((*q)->all_variables) > sylvan_var((*p)->all_variables)) {
+            t = *q;
+            *q = *p;
+            *p = t;
+            if (--i) continue;
+        }
+        i = j++;
+    }
+
+    // Get next id (for cache)
+    static volatile int id = 0;
+    int _id = __sync_fetch_and_add(&id, 1);
+
+    // Go!
+    LACE_ME;
+    dst->bdd = CALL(go_sat, src->bdd, rels, rel_count, _id);
+}
+
 static void
 set_reorder()
 {
@@ -1209,7 +1313,7 @@ dom_set_function_pointers(vdom_t dom)
     dom->shared.rel_count=rel_count;
     dom->shared.set_next=set_next;
     dom->shared.set_prev=set_prev;
-    // set_least_fixpoint
+    dom->shared.set_least_fixpoint=set_least_fixpoint;
 
     dom->shared.reorder=set_reorder;
     dom->shared.set_dot=set_dot;
