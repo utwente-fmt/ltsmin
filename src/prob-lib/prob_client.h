b486d270cc0aa95a4f94902bbc5d8d3a0ef544c4
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2018-03-06 12:43:30 +0100

    try to implement NextStateShortR2W

--- src/prob-lib/prob_client.h
+++ src/prob-lib/prob_client.h
@@ -12,6 +12,7 @@ extern void prob_client_destroy(prob_client_t pc);
 extern const char* prob_get_zocket(prob_client_t pc);
 
 extern ProBState* prob_next_state(prob_client_t pc, ProBState s, char *transitiongroup, int *size);
+extern ProBState* prob_next_state_short_R2W(prob_client_t pc, ProBState s, char *transitiongroup, int *size);
 extern ProBState* prob_next_action(prob_client_t pc, ProBState s, char *transitiongroup, int *size);
 
 extern void prob_terminate(prob_client_t pc);
@@ -27,6 +28,7 @@ extern ProBInitialResponse prob_init(prob_client_t pc, int is_por);
 extern void print_matrix(const ProBMatrix m);
 
 extern int prob_get_state_label(prob_client_t pc, ProBState s, char *label);
+extern int prob_get_state_label_short(prob_client_t pc, ProBState s, char *label);
 
 void prob_get_label_group(prob_client_t pc, ProBState s, int group, int *res);
 #endif
