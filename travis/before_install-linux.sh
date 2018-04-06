47cfed37f687b17be0d196c870764617a02bedbc
Jeroen Meijer <jjgmeijer@gmail.com>
2018-04-06 13:12:42 +0200

    Dump contents of cache.

--- travis/before_install-linux.sh
+++ travis/before_install-linux.sh
@@ -2,6 +2,11 @@
 set -e
 #set -o xtrace
 
+ls -al "$HOME/ltsmin-deps" || true
+ls -al "$HOME/ltsmin-deps/lib" || true
+ls -al "$HOME/ltsmin-deps/include" || true
+ls -al "$HOME/ltsmin-deps/lib/pkgconfig" || true
+
 true
 
 set +e
