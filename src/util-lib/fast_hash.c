7c1cdb6b2e80f6a4e620786ac0b1596bef28efc5
Jeroen Meijer <jjgmeijer@gmail.com>
2018-06-19 14:04:36 +0200

    Fix fall through without pragma.

--- src/util-lib/fast_hash.c
+++ src/util-lib/fast_hash.c
@@ -5,8 +5,6 @@
 
 #include <util-lib/fast_hash.h>
 
-#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
-
 #undef get16bits
 #if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
   || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
@@ -160,12 +158,12 @@ uint64_t MurmurHash64 ( const void * key, int len, unsigned int seed )
 
     switch(len & 7)
     {
-    case 7: h ^= ((uint64_t)data2[6]) << 48;
-    case 6: h ^= ((uint64_t)data2[5]) << 40;
-    case 5: h ^= ((uint64_t)data2[4]) << 32;
-    case 4: h ^= ((uint64_t)data2[3]) << 24;
-    case 3: h ^= ((uint64_t)data2[2]) << 16;
-    case 2: h ^= ((uint64_t)data2[1]) << 8;
+    case 7: h ^= ((uint64_t)data2[6]) << 48; // fall through
+    case 6: h ^= ((uint64_t)data2[5]) << 40; // fall through
+    case 5: h ^= ((uint64_t)data2[4]) << 32; // fall through
+    case 4: h ^= ((uint64_t)data2[3]) << 24; // fall through
+    case 3: h ^= ((uint64_t)data2[2]) << 16; // fall through
+    case 2: h ^= ((uint64_t)data2[1]) << 8; // fall through
     case 1: h ^= ((uint64_t)data2[0]);
             h *= m;
     };
@@ -213,8 +211,8 @@ uint64_t MurmurHash64 ( const void * key, int len, unsigned int seed )
 
     switch(len)
     {
-    case 3: h2 ^= ((unsigned char*)data)[2] << 16;
-    case 2: h2 ^= ((unsigned char*)data)[1] << 8;
+    case 3: h2 ^= ((unsigned char*)data)[2] << 16; // fall through
+    case 2: h2 ^= ((unsigned char*)data)[1] << 8; // fall through
     case 1: h2 ^= ((unsigned char*)data)[0];
             h2 *= m;
     };
