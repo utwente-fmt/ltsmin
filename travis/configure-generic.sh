c0c31f99cb6dffc8c1fa3e6e1a087f3874350127
Jeroen Meijer <jjgmeijer@gmail.com>
2018-08-09 10:33:15 +0200

    Fix formatting

--- travis/configure-generic.sh
+++ travis/configure-generic.sh
@@ -5,7 +5,8 @@ set -e
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOME/ltsmin-deps/lib/pkgconfig"
 
 CONFIGURE_FLAGS="-q --with-viennacl=$HOME/ltsmin-deps/include --enable-werror"
-CONFIGURE_FLAGS="$CONFIGURE_FLAGS --enable-silent-rules --disable-scoop"
+CONFIGURE_FLAGS="$CONFIGURE_FLAGS --enable-silent-rules"
+CONFIGURE_FLAGS="$CONFIGURE_FLAGS --disable-scoop"
 CONFIGURE_FLAGS="$CONFIGURE_FLAGS --disable-dependency-tracking"
 export CONFIGURE_FLAGS="$CONFIGURE_FLAGS --with-mcrl2=$HOME/ltsmin-deps$MCRL2_LIB_DIR $@"
 
