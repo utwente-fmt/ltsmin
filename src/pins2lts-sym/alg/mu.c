a63a568d9636b4986547a9509cdbeb910c466bf8
Codermann63 <96649204+Codermann63@users.noreply.github.com>
2023-12-11 08:28:57 +0100

    changed refferences from aux to auxiliary to increase compatability with windows

--- src/pins2lts-sym/alg/mu.c
+++ src/pins2lts-sym/alg/mu.c
@@ -19,9 +19,9 @@
 #include <pins-lib/pins2pins-group.h>
 #include <pins-lib/property-semantics.h>
 #include <pins2lts-sym/alg/mu.h>
-#include <pins2lts-sym/alg/aux.h>
-#include <pins2lts-sym/aux/options.h>
-#include <pins2lts-sym/aux/prop.h>
+#include <pins2lts-sym/alg/auxiliary.h>
+#include <pins2lts-sym/auxiliary/options.h>
+#include <pins2lts-sym/auxiliary/prop.h>
 #include <ltsmin-lib/ltsmin-standard.h>
 #include <ltsmin-lib/ltsmin-syntax.h>
 #include <ltsmin-lib/ltsmin-tl.h>
